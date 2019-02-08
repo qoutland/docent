6af9569d02b8045efe093344f08aa10e69e42080
Quin Outland <qoutland@gmail.com>
2019-02-08 14:28:13 -0800

    Fixed my mess

--- docent/settings.py
+++ docent/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'activity.apps.ActivityConfig',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -44,10 +45,24 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',  #GAuth
+                'social_django.context_processors.login_redirect', #GAuth
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+ 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+ 'social_core.backends.google.GoogleOpenId',  # for Google authentication
+ 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+ 'social_core.backends.github.GithubOAuth2',  # for Github authentication
+ 'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
+ 'django.contrib.auth.backends.ModelBackend',
+)
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='585163855279-r6at10r3g9qfl7cgo01eqt3rhvglqo1c.apps.googleusercontent.com'  #Paste CLient Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '66sEFfjJKDJ4u9H8qGmumXvH' #Paste Secret Key
 
 WSGI_APPLICATION = 'docent.wsgi.application'
 
@@ -80,6 +95,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/activity'
 LOGOUT_REDIRECT_URL = '/activity'
 APPEND_SLASH = True
