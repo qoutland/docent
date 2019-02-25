401f0ab5b107b32346b07b8172c4206c83769980
Outland <QOutland@nvenergy.com>
2019-02-25 13:43:26 -0800

    star animation

---
+++ static/js/app.js
@@ -0,0 +1,23 @@
+$('.likebutton').click(function () {
+    var catid;
+    catid = $(this).attr("data-catid");
+    $.ajax({
+      type: "GET",
+      url: "save_act",
+      data: {
+        post_id: catid
+      },
+      success: function (data) {
+        var str = "." + catid;
+        if (data == 'created') {
+          $(str).find("span").fadeOut("fast")
+          $(str).find("span").fadeIn()
+          $(str).find("span").removeClass("glyphicon-star-empty").addClass("glyphicon-star");
+        } else if (data == 'deleted') {
+          $(str).find("span").fadeOut("fast")
+          $(str).find("span").removeClass("glyphicon-star").addClass("glyphicon-star-empty");
+          $(str).find("span").fadeIn()
+        }
+      },
+    })
+  });
