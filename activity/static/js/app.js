c7c79bb9d1bf948f2488a9f656c18719125fa12e
qoutland <qoutland@gmail.com>
2019-02-26 14:29:29 -0800

    Adding jquery/ajax for moving activities in profile

--- activity/static/js/app.js
+++ activity/static/js/app.js
@@ -10,13 +10,31 @@ $('.likebutton').click(function () {
       success: function (data) {
         var str = "." + catid;
         if (data == 'created') {
-         $(str).find("span").fadeOut("fast")
+          $(str).find("span").fadeOut("fast")
           $(str).find("span").fadeIn()
           $(str).find("span").removeClass("glyphicon-star-empty").addClass("glyphicon-star");
         } else if (data == 'deleted') {
           $(str).find("span").fadeOut("fast")
           $(str).find("span").removeClass("glyphicon-star").addClass("glyphicon-star-empty");
           $(str).find("span").fadeIn()
+        }
+      },
+    })
+  });
+
+  $('.removeLike').click(function () {
+    var catid;
+    catid = $(this).attr("data-catid");
+    $.ajax({
+      type: "GET",
+      url: "save_act",
+      data: {
+        post_id: catid
+      },
+      success: function (data) {
+        var str = "." + catid;
+          if (data == 'deleted') {
+          $(str).parent().parent().fadeOut()
         }
       },
     })
