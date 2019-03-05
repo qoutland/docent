07b735e6d5b6955cfbfd19a9e7741dd54ed59d90
Quin Outland <qoutland@gmail.com>
2019-03-05 09:23:34 -0800

    Yelpin

--- api/yelper2.py
+++ api/yelper2.py
@@ -14,8 +14,5 @@ def toJSON(result):
 
 yelp_api = YelpAPI('0X8SzKkV2v7bo9s_vUvl7IR23KFICRqBaucXJ9DOYQlhgDqXgOeZuzk3ruirXyphW0O6cZXrQfzJRgaHREFQNiBIDXzmwDUvgWdNBQRGLezZ4h7a1D4G8H8Wi-e3W3Yx', timeout_s=3.0)
 search_results = yelp_api.search_query(term='Things To Do', location='reno, nv', sort_by='rating', limit=10)
-'''
-print(json.dumps(toJSON(search_results), sort_keys=True, indent=4, separators=(',',': ')))
+#print(json.dumps(toJSON(search_results), sort_keys=True, indent=4, separators=(',',': ')))
 print(json.dumps(toJSON(search_results), sort_keys=True, indent=4, separators=(',',': ')), file=open("top10.json", "w"))
-
-'''
