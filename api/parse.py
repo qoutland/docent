24d9ac6da7bcaa47679d55e3f6e12f0d2c2c307f
Quin Outland <qoutland@gmail.com>
2019-03-05 12:58:07 -0800

    updating db and other stuff

--- api/parse.py
+++ api/parse.py
@@ -1,10 +1,25 @@
 import json
 from pprint import pprint
-
+from activity.models import Activity
 with open('top10.json','r') as f:
     data = json.load(f)
 
 for act in data['businesses']:
+    Activity.objects.create(
+        name=act['name'],
+        phone_number=act['phone'],
+        display_phone=act['display_phone'],
+        url=act['url'],
+        pic_url=act['image_url'],
+        longitude=act['location']['longitude'],
+        latitude=act['location']['latitude'],
+        address1=act['location']['address1'],
+        address2=act['location']['address2'],
+        city=act['location']['city'],
+        state=act['location']['state'],
+        code=act['location']['zip_code']
+    )
+
     print(act['name'])
     #print(act['coordinates']['latitude'])
     #print(act['coordinates']['longitude'])
