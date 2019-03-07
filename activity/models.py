adc6acb4a2d5aabee840384a572927072d93157b
Quin Outland <qoutland@gmail.com>
2019-03-06 17:54:59 -0800

    added review

--- activity/models.py
+++ activity/models.py
@@ -21,6 +21,7 @@ class Interest(models.Model): #An entry for a user and an ActivityType
 class Activity(models.Model): #An activity object (using a PK for individual references)
 	ID = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=250)
+	avg_review = models.FloatField(null=True)
 	activity_type = models.ManyToManyField(ActivityType, through='ActivityTypeLine')
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
