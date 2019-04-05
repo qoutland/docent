3a8090567821a562cbb3b4779538e3d6f0028837
= <=>
2019-04-05 15:08:35 -0700

    Fixed search query/pills/removed category

--- activity/urls.py
+++ activity/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('profile/', views.profile, name='profile'),
 	path('signup', views.signUp, name='signup'),
-    path('category', views.category, name='category'),
     path('save_act/', views.saveAct, name="save_act"),
     path('profile/save_act/', views.saveAct, name="save_act"),
     path('profile/new_interest/', views.add_removeInterest, name="new_interest"),
