c278d4900c7681642c0fedef2b8988964a667808
= <=>
2019-04-08 13:42:09 -0700

    Allow a user to delete an account

--- activity/urls.py
+++ activity/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile/', views.profile, name='profile'),
+    path('profile/delete', views.delete_profile, name='delete'),
 	path('signup', views.signUp, name='signup'),
     path('save_act/', views.saveAct, name="save_act"),
     path('profile/save_act/', views.saveAct, name="save_act"),
